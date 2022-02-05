d8f76a23b34205e1c6dc468e08df26a60f003e9c
Milansuman <milansuman2016@gmail.com>
2022-02-06 00:05:54 +0530

    more code

---
+++ arch.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+class Person:
+    def __init__(self, name="", title="", home_unit=""):
+        self.name = name
+        self.title = title
+
+def filter(element, attr, search):
+    return lambda tag: tag.name == element and attr in tag.attrs and search in tag[attr]
+
+
+resp = requests.get("https://arch.gatech.edu/people?field_category_value=faculty")
+soup = BeautifulSoup(resp.content, "html.parser")
+
+people = []
+for elem in soup.find_all(filter("div", "class", "profile-card")):
+    person = Person()
+    for name_data in elem.find_all(filter("h3", "class", "m-auto")):
+        person.name = name_data.text
+    for title_data in elem.find_all(filter("div", "class", "card-text")):
+        person.title = title_data.text
+    people.append(person.__dict__)
+
+with open("architecture.csv", "w") as file:
+    fieldnames = ["name", "title"]
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    for person in people:
+        writer.writerow(person)
--- computing.py
+++ computing.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-from tabulate import tabulate
+import csv
 
 class Person:
     def __init__(self, name="", title="", home_unit=""):
@@ -34,5 +34,11 @@ cyber_sec = req(3, 292, "School of Cybersecurity and Privacy")
 int_computing = req(8, 205, "School of Interactive Computing")
 
 units = [compute_inst, compute_sci_and_engineering, comp_sci, cyber_sec, int_computing]
-for unit in units:
-    print(tabulate(unit))
+
+with open("computing.csv", "w") as file:
+    fieldnames = ["name", "title", "home_unit"]
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    for unit in units:
+        for row in unit:
+            writer.writerow(row)
