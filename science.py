81ebd1ffd8ecd14bff8e14c1edbc9d35c69389bc
Milansuman <milansuman2016@gmail.com>
2022-02-06 11:22:52 +0530

    word wrap science.py

--- science.py
+++ science.py
@@ -1,3 +1,5 @@
+from re import L
+from turtle import title
 from bs4 import BeautifulSoup
 import requests
 from tabulate import tabulate
@@ -10,6 +12,10 @@ class Person:
 def filter(element, attr, search):
     return lambda tag: tag.name == element and attr in tag.attrs and search in tag[attr]
 
+def wrap(string: str):
+    limit = int(len(string)/3)
+    return string[:limit] + "\n" + string[limit:limit*2] + "\n" + string[(limit*2):]
+
 
 resp = requests.get("https://biosciences.gatech.edu/people?field_last_name_value=&field_job_category_tid=All")
 soup = BeautifulSoup(resp.content, "html.parser")
@@ -20,6 +26,9 @@ for elem in soup.find_all(filter("li", "class", "biosci-people-cell")):
     for name_data in elem.find_all(filter("span", "class", "p-name")):
         person.name = name_data.text
     for title_data in elem.find_all(filter("span", "class", "p-job-title")):
+        if len(title_data.text) > 100:
+            person.title = wrap(title_data.text)
+            break
         person.title = title_data.text
     if person.title == "":
         continue
