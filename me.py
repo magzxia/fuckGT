1ea100149d4776590252fd05be526d2fdcb05446
Milansuman <milansuman2016@gmail.com>
2022-02-06 10:11:35 +0530

    finish me ;)

---
+++ me.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+from tabulate import tabulate
+
+class Person:
+    def __init__(self, name="", title=""):
+        self.name = name
+        self.title = title
+
+def filter(element, attr, search):
+    return lambda tag: tag.name == element and attr in tag.attrs and search in tag[attr]
+
+def req(pages) -> list:
+    lst = []
+    for i in range(pages):
+        resp = requests.get(f"https://www.me.gatech.edu/staff/all?field_last_name_value=&field_staff_group_target_id=All&field_all_research_areas_target_id=All&page={i}")
+        soup = BeautifulSoup(resp.content, "html.parser")
+
+        for elem in soup.find_all(filter("div", "class", "faculty__user-details")):
+            person = Person()
+            for name_data in elem.find_all(filter("a", "href", "/faculty/")):
+                person.name = name_data.text
+            for title_data in elem.find_all(filter("div", "class", "faculty-title")):
+                person.title = title_data.text
+
+            lst.append(person.__dict__)
+    return lst
+
+
+people = req(12)
+print(tabulate(people, tablefmt="grid"))
