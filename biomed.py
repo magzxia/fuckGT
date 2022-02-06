1fd1a52a2703e57d571c1f9894ae115f132f16f2
Milansuman <milansuman2016@gmail.com>
2022-02-06 08:51:38 +0530

    minor fixes

--- biomed.py
+++ biomed.py
@@ -15,9 +15,9 @@ resp = requests.get("https://bme.gatech.edu/bme/faculty")
 soup = BeautifulSoup(resp.content, "html.parser")
 
 people = []
-for elem in soup.find_all(filter("div", "class", "views-field-field-last-name")):
+for elem in soup.find_all(filter("div", "class", "views-col")):
     person = Person()
-    for name_data in elem.find_all(filter("div", "class", "fac-name")):
+    for name_data in elem.find_all(filter("div", "class", "views-field-field-last-name")):
         person.name = name_data.text
     for title_data in elem.find_all(filter("div", "class", "views-field-field-title")):
         person.title = title_data.text
